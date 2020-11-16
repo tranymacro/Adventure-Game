2dc6d917285b2e61080df59152eb1e3a6f301715
Walter <72188175+tranymacro@users.noreply.github.com>
2020-11-15 20:52:47 -0600

    Add files via upload

---
+++ The room of keys/The room of keys/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace The_room_of_keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameTitle();
+            PlayerWakeUpEv1();
+        }
+        //madebyWalterCooper
+        public static void GameTitle()
+        {
+            Console.WriteLine("   ▄▄▄▄▀ ▄  █ ▄███▄       █▄▄▄▄ ████▄ ████▄ █▀▄▀█     ████▄ ▄████      █  █▀ ▄███▄ ▀▄    ▄  ▄▄▄▄▄   ");
+            Console.WriteLine("▀▀▀ █   █   █ █▀   ▀      █  ▄▀ █   █ █   █ █ █ █     █   █ █▀   ▀     █▄█   █▀   ▀  █  █  █     ▀▄ ");
+            Console.WriteLine("    █   ██▀▀█ ██▄▄        █▀▀▌  █   █ █   █ █ ▄ █     █   █ █▀▀        █▀▄   ██▄▄     ▀█ ▄  ▀▀▀▀▄   ");
+            Console.WriteLine("   █    █   █ █▄   ▄▀     █  █  ▀████ ▀████ █   █     ▀████ █          █  █  █▄   ▄▀  █   ▀▄▄▄▄▀    ");
+            Console.WriteLine("  ▀        █  ▀███▀         █                  █             █           █   ▀███▀  ▄▀              ");
+            Console.WriteLine("          ▀                ▀                  ▀               ▀         ▀                           ");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Made by WalterCooper");
+            Console.WriteLine("press 'Enter' to Start");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        
+
+        public static void PlayerWakeUpEv1()
+        {
+            
+
+
+            Console.WriteLine("Dark");
+            Console.ReadLine();
+
+            Console.WriteLine("Darkness suround your being");
+            Console.ReadLine();
+
+            Console.WriteLine("You look left and you see nothing but a void of inky blackness,\n" +
+                "as you look right your greeted with the same black abyess");
+            Console.ReadLine();
+
+            Console.WriteLine("Even thought you can see nothing but black, you feel something");
+            Console.ReadLine();
+
+            Console.WriteLine("  .              +   .            .   . .     .  . \n"+
+                    "  .                    .       .     *    \n"+
+                  "   .       *                        . . . .  .   .  + .\n" +
+              " .   .  +  . . .                                              \n" +
+               " .                 |             .  .   .    .    . .       \n" +
+                  "            .     .     . +.    +  .                     \n" +
+                    "         .       .   . .                               \n" +
+                 " . ..    * . . .  .  +   .                                  \n" +
+                " +      .           .   .      +                            \n" +
+                "            .       . +  .+. .                              \n" +
+              " .                      .     . + .  . .     .      .           \n" +
+              "      .      .    .     . .   . . .        ! /              \n" +
+               "   *             .    . .  +    .  .       - O -               \n" +
+            " .     .    .  +   . .  *  .       . / |                       \n" +
+             "  . + .  .  .  ..+  .                                          \n" +
+            "  .      .  .  .  *   .  *  . + ..  .            *                \n" +
+              " .      .   . .   .   .   . .  +   .    .            + ");
+            Console.WriteLine("\n-------------------------------------------------------------------------------");
+            Console.WriteLine("\n-------------------------------------------------------------------------------");
+
+            Console.WriteLine("You can't quite define the shape at first, but after\n" +
+                " a while of feeling a around the object it starts to being more familiar");
+            Console.ReadLine();
+
+            Console.WriteLine("Press Enter To Continue");
+            Console.ReadLine();
+            Console.Clear();
+
+            string Choice1OfEv1;
+
+            Console.WriteLine(" `.``.````.````.``.`.``.`.``.`.``.`.``.`.``.`.``.````.````.````.````.``.`.``\n" +
+                              " ````.````.````.``.`.````.``.`.``.`.``.`.``.`.``.````.````.````.````.````.``\n" +
+                              " ```````````````````````````````````````````````````````````````````````````\n" +
+                              " .``.`.``.`.````.``.`.``.`.``.`..`.````.``.`.``.`.``.`.``.`.``.````.`.``.```\n" +
+                              " .``.````.`.``.``````..`````.-/:::::-.+//../::::-.``.`.``.````.`.``.`.``````\n" +
+                              " `````````````````.-`-.``````.++++++/osooo.-/////-```.`````````.````````````\n" +
+                              " `````````.`````..-+++:.`````.+++///+yyysy+-.-///:```.`````````.````````````\n" +
+                              " `````````.`````.:++oo++:.```.++//syyyyhyyyyo-.//:````````.`````````````````\n" +
+                              " ``````````````.-++//+/++/````+/+yyyyyshyyyyyy:./:``````````````````````````\n" +
+                              " ````````.````.:/+/.`../++::::+oyyyyo/+ooosyyys:o:``````````````````````````\n" +
+                              " ``````````````-/+----.-++/::/osyyys-ossssssyyh:o:``````````````````````````\n" +
+                              " `````````````.-/+.---../o/..-+syyys-oosssosyyh:o/``````````````````````````\n" +
+                              " `````````.````-:+/...`:++////osyyyy+/+ooooyyys:+-```.````.````.````.```````\n" +
+                              " ````````````````/+/://+++-```//oyyyyssysyyyyy-./:````````.`````````````````\n" +
+                              " `````````````````:++o+++:````/+/+syyyyhyyyyo-./+/...........```````````````\n" +
+                              " ````````.````.````-////-`````/++//+oysysy+-.-/++/..-.-..-.-........`.``````\n" +
+                              " .````.``.`.......`..`:.``````/++++//+://o../++++/.--.----.--.-.....`.``.```\n" +
+                              " ````.````.`.......-..:-....../+++ooo:-:---://::-..........`..`.````.````.``\n" +
+                              " ````.````.````.``.`.....-.--.::::---.......`.``.````.````.````.````.````.``\n" +
+                              " ``````````````````````````..`.``.``````````````````````````````````````````\n" +
+                              " .``.`.``.`.``.`.``.`.``.`.``.````.`.``.``.`.``.`.``.`.``.`.``.`.``.`.``.```\n" +
+                              " .````.``.`.``.`.``.`.``.`.``.````.````.````.``.`.``.`.``.`.``.````.````.``` ");
+            Console.WriteLine("\n------------------------------------------------------------------------------------------");
+            Console.WriteLine("\n------------------------------------------------------------------------------------------");
+            Console.WriteLine("As you felt around the object you finally what it is");
+            Console.ReadLine();
+            
+            Console.WriteLine("The object appears to be a cube, but not only that\nit appears to have a butten on the back of it");
+            Console.ReadLine();
+
+            Console.Write("Now knowing what the object is\nWhat should you do?");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter 1 to press the butten\nEnter 2 to Wait\nEnter 3 to throw the cube back in to the void");
+
+            Choice1OfEv1 = Console.ReadLine().ToLower();
+            Console.Clear();
+
+            switch (Choice1OfEv1)
+            {
+                case "1":
+                case "press the the butten":
+                case "butten":
+                    {
+                        Console.WriteLine("You press the butten, on the mysterous object, at first noting happens but then you heard a voice");
+                        Console.WriteLine("You could not find where it was coming from, but it sound like it was in your head");
+                        Console.ReadLine();
+
+                        Console.WriteLine("The voice is deep and rough and sounded as if it was not you but\nyet spoke a thousand one word.");
+                        Console.WriteLine("As you finally located the voice, you wish you didn't" +
+                            "\nbecome along with the voice came a skeleton disfured face\n with a body of which you can not describe");
+                        Console.ReadLine();
+
+                        Console.WriteLine("The figure kept repeating a ryhme that you never of, over and over again like a endless mantra.");
+                        Console.WriteLine("You can barely hear it, but as you focused on the voice the you can finally decipher what is saying");
+                        Console.ReadLine();
+
+                        Console.WriteLine("The voice say");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&%% (#%##(((((//#%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@% (/*/(/,,,,,,.,,//&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&%/*,,*,,,,,,*,,*(###%&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&%#*,,,/,*,.,,***(#%%#%&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&##,..,***,,,*,,,*(#(((###%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&(#*...,.,,,.,.,,,**(*//#%#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&@##/*%#/&&*,,,.,(*/###&&&@&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@% (*(&@@&@@&% (*#,,%@@&&&&%&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&(./*#%&&&@%(,,,,(&&&#&&@%%&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&/*,/%&&/*,*//#@&&%&%#(%&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&%@@&%% (#%#%&%&@&&#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&@&%@@@%@%% (@% &/ &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&@@@&&/.,, (&%%#/,(@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@% &@@&@@&@%,,@#.%@@@@##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&**, ((%, &,%@@/ (/ &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@**,,.*.* *&@#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                                          "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+
+                        Console.WriteLine("\n------------------------------------------------------------------------------------------");
+                        Console.WriteLine("\n------------------------------------------------------------------------------------------");
+                        Console.WriteLine("A candle light ");
+
+
+                        Console.ReadLine();
+
+                        Console.WriteLine("");
+                    }
+
+            }
+
+
+
+
+
+
+
+        }
+
+        public static void PlayerEvent1()
+        {
+
+        }
+
+        public static void PlayerEvent2()
+        {
+
+        }
+
+        public static void PlayerEvent3()
+        {
+
+        }
+
+        public static void PlayerEvent4()
+        {
+
+        }
+
+        public static void PlayerEvent5()
+        {
+
+        }
+
+        public static void PlayerGameOverEv1OfW1()
+        {
+
+        }
+
+        public static void PlayerWinEv1OfW1()
+        {
+
+        }
+
+
+
+    }
+}
